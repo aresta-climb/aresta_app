97ff5c6e2a040845970b385c85610aaf848c02e6
Eduardo Utsch Gonçalves <utscheduardo@gmail.com>
2026-06-12 21:35:29 -0300

    Dart fix

--- frontend/test/navigation/page_listenable_builder_test.dart
+++ frontend/test/navigation/page_listenable_builder_test.dart
@@ -4,7 +4,6 @@ import 'package:frontend/aresta_api/proto/generated/croqui.pb.dart';
 import 'package:frontend/navigation/page_listenable_builder.dart';
 import 'package:frontend/services/dataset_repository.dart';
 import 'package:frontend/services/editor_croqui.dart';
-import 'package:frontend/navigation/navigation_functions.dart';
 
 // Mock observer para testar se AppNav.back() foi chamado
 class MockNavigatorObserver extends NavigatorObserver {
