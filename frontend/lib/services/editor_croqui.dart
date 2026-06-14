e5382eff49e5afd576093f43d1037485ecf9ceef
Eduardo Utsch Gonçalves <utscheduardo@gmail.com>
2026-06-14 17:17:11 -0300

    fix(dataset): garante metadados de inicialização e corrige bug de url experimental

--- frontend/lib/services/editor_croqui.dart
+++ frontend/lib/services/editor_croqui.dart
@@ -225,6 +225,9 @@ class EditorDeCroqui {
 
     isExperimentalMode.value = true;
     if (url != null) {
+      if (url.isNotEmpty && !url.contains('://')) {
+        url = 'http://$url';
+      }
       editorUrl.value = url;
     }
 
