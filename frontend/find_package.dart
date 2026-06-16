0535fa9cb997efc884c7ccc43bcc77d549b64fc0
Renato Utsch <renatoutsch@gmail.com>
2026-06-16 19:46:28 -0300

    Delete temp file

--- frontend/find_package.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:isolate';
-import 'package:feedback/feedback.dart';
-
-void main() async {
-  final uri = await Isolate.resolvePackageUri(Uri.parse('package:feedback/feedback.dart'));
-  print('Feedback package path: $uri');
-}
