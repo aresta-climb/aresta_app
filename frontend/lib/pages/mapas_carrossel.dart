c81fcf59d01d1ee209d7f27dc2d2b42f2e5a9e19
Eduardo Utsch Gonçalves <utscheduardo@gmail.com>
2026-07-06 16:13:11 -0300

    Dart fix

--- frontend/lib/pages/mapas_carrossel.dart
+++ frontend/lib/pages/mapas_carrossel.dart
@@ -134,7 +134,7 @@ class _MapasCarrosselPageState extends State<MapasCarrosselPage> {
                   filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
                   child: Container(
                     decoration: BoxDecoration(
-                      color: Colors.black.withOpacity(0.5),
+                      color: Colors.black.withValues(alpha: 0.5),
                       borderRadius: BorderRadius.circular(24),
                       border: Border.all(color: Colors.white24),
                     ),
