2ff84c8993aaea4ab269c51de4a370c7829bca54
Eduardo Utsch Gonçalves <utscheduardo@gmail.com>
2026-07-21 23:46:49 -0300

    test: adiciona teste pro delegate de busca do pico e atualiza hint

--- frontend/test/view_functions/pico_functions_test.dart
+++ frontend/test/view_functions/pico_functions_test.dart
@@ -1,5 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:frontend/services/firebase/telemetry_service.dart';
+import 'package:frontend/view_functions/pico_functions.dart';
+import 'package:frontend/view_functions/via_functions.dart';
+import 'package:frontend/aresta_api/proto/generated/croqui.pb.dart';
 import '../mocks/mock_telemetry_service.dart';
 
 void main() {
@@ -28,5 +31,29 @@ void main() {
     TelemetryService.instance.logAcaoCroqui('crag1', 'Sobre o Pico');
     expect(mockTelemetry.recordedEvents, contains('acao_croqui'));
     expect(mockTelemetry.recordedParams['acao_croqui']!['acao'], 'Sobre o Pico');
+  });
+
+  test('PicoSearchDelegate fetches setores and escaladas and sets correct label', () {
+    final pico = Pico()
+      ..setoresOuGrupos.add(SetorOuGrupo()
+        ..setor = (ArquivoSetor()
+          ..conteudo = (Setor()
+            ..nome = 'Setor A'
+            ..escaladas.add(Escalada()
+              ..viaEsportiva = (ViaEsportiva()..nome = 'Via 1')
+            )
+          )
+        )
+      );
+
+    final delegate = PicoSearchDelegate(pico, 'crag1');
+    
+    expect(delegate.allEscaladas.length, 1);
+    expect(getEscaladaNome(delegate.allEscaladas.first), 'Via 1');
+    
+    expect(delegate.allSetores.length, 1);
+    expect(delegate.allSetores.first.nome, 'Setor A');
+    
+    expect(delegate.searchFieldLabel, 'Buscar escalada (ex: 7a) ou setor...');
   });
 }
